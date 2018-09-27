f088424e3fb89c0513e2bc26c5a64a61aeae8589
Arjun Jhawar <arjun.jhawar@gmail.com>
2018-09-28 09:00:14 +1200

    fix cucumberJs launch problem

--- cucumber.js
+++ cucumber.js
@@ -10,7 +10,7 @@ const profileName = argv.profile || 'default';
 const outputDir = `./testoutput/${profileName}`;
 fs.emptyDir(outputDir);
 const targetResultJson = `${outputDir}/${profileName}.json`;
-const commonOptions = `--format json:${targetResultJson} --format summary --require-module ts-node/register --require tests/**/*.ts --fail-fast`;
+const commonOptions = `--format json:${targetResultJson} --format summary --require-module ts-node/register --require tests/step_definitions/*.ts --fail-fast`;
 const common = `${commonOptions}`;
 
 makeDir(outputDir);
